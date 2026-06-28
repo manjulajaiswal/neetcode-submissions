8b07a7ca3a0c704adfcd0b9b4f21548ab4d6d34b
ManjulaJaiswal <112776707+manjulajaiswal@users.noreply.github.com>
2026-06-29 05:26:27 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l =0, r =1;
+        int maxProfit =0;
+
+        while(r < prices.size()){
+            if(prices[l] < prices[r]) {
+                int pro = prices[r] - prices[l];
+                maxProfit = max(maxProfit, pro);
+            }
+            else {
+                l = r;
+            }
+            r++;
+        }
+        return maxProfit;
+    }
+};
